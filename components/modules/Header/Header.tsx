ee135cc369149b2f7bf1ba058f9aa8c61f74344e
Nikolay Pilgun <a03198814s@gmail.com>
2024-08-19 14:33:01 +0500

    start

---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => (
+  <header className='header'>
+    <div className='container header__container'>
+      <button className='btn-reset header__burger'>Header</button>
+    </div>
+  </header>
+)
+
+export default Header
---
+++ hooks/useLang.ts
@@ -0,0 +1,10 @@
+import { $lang } from '@/context/lang'
+import translationsJson from '@/public/translations/translations.json'
+import { useUnit } from 'effector-react'
+
+export const useLang = () => {
+  const lang = useUnit($lang)
+  const translations = translationsJson
+
+  return { lang, translations }
+}
